d8701b95d7ee90b9a69f2b944fd7a3905c9307f8
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-10-23 17:41:01 +0530

    new file:   client/.eslintrc.json 	new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/components.json 	new file:   client/next.config.mjs 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/postcss.config.mjs 	new file:   client/src/app/favicon.ico 	new file:   client/src/app/fonts/GeistMonoVF.woff 	new file:   client/src/app/fonts/GeistVF.woff 	new file:   client/src/app/globals.css 	new file:   client/src/app/layout.tsx 	new file:   client/src/app/page.tsx 	new file:   client/src/lib/utils.ts 	new file:   client/tailwind.config.ts 	new file:   client/tsconfig.json 	modified:   server/package-lock.json 	modified:   server/package.json 	modified:   server/src/app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,12 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import passport from 'passport';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import "./config/passport";
+
+import authRoute from "./routes/auth";
 
 const app = express();
 
@@ -15,6 +21,23 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.MONGODB_URI!}),
+    cookie: {
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 24 * 60 * 60 * 1000, 
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoute);
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
